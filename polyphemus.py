96af5e582a1c2955caa74bb18c32a290c453b97d
squilter <s.quilter@aol.com>
2014-06-24 20:03:37 -0700

    try/catch only catches 0division, not any exception

--- polyphemus.py
+++ polyphemus.py
@@ -3,7 +3,6 @@
 import numpy as np
 import cv2
 
-
 def main():
     cam = cv2.VideoCapture(0)
     hist = np.array([[255.], [0.], [0.], [0.], [0.], [0.], [0.], [0.], [0.], [0.], [0.], [0.], [0.], [0.], [0.], [255.]])
@@ -26,9 +25,6 @@ def detect_target(cv2, hist, np, frame):
     binary_img = clean_up_with_morphology(cv2, prob)
     biggest_contour = detect_biggest_polygon(cv2, binary_img)
     return biggest_contour
-
-
-
 
 def filter_red_pixels(cv2, hist, np, frame):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -68,20 +64,14 @@ def render_crosshairs(cv2, frame, biggest_contour):
     if biggest_contour != None:
         green = 20, 255, 60
         contour_centroid = cv2.moments(biggest_contour)
-        print contour_centroid
         
         try:
             cx, cy = int(contour_centroid['m10'] / contour_centroid['m00']), int(contour_centroid['m01'] / contour_centroid['m00'])
             cv2.circle(frame, (cx, cy), 4, green, 5)
             cv2.line(frame, (0, cy), (frame.shape[1], cy), green, 1)
             cv2.line(frame, (cx, 0), (cx, frame.shape[0]), green, 1)
-        except:
-            print "error caught"
+        except ZeroDivisionError:
             pass
-
-
-
 
 if __name__ == '__main__':
     main()
-        
