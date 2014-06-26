fbbe46083e0b909ea18e33a34866364c440f9364
squilter <s.quilter@aol.com>
2014-06-26 11:02:49 -0700

    video recording is optional parameter now
    use --record to record output to an avi file

--- polyphemus.py
+++ polyphemus.py
@@ -3,6 +3,7 @@
 import numpy as np
 import cv2
 import glob
+import sys
 
 def main():
     cam = cv2.VideoCapture(0)
@@ -10,9 +11,11 @@ def main():
     hist = hist.astype(np.float32, copy=False)
     while True:
         frame = get_frame(cam)
-        writer.write(frame)
-#         biggest_contour = detect_target(cv2, hist, np, frame)
-#         render_crosshairs(cv2, frame, biggest_contour)
+        if record:
+            print "recording"
+            writer.write(frame)
+        biggest_contour = detect_target(cv2, hist, np, frame)
+        render_crosshairs(cv2, frame, biggest_contour)
         cv2.imshow("frame", frame)
         ch = 0xFF & cv2.waitKey(5)
         if ch == 27:
@@ -76,9 +79,11 @@ def render_crosshairs(cv2, frame, biggest_contour):
             pass
 
 if __name__ == '__main__':
+    record = len(sys.argv) >= 2 and sys.argv[1] == "--record"
     video_counter = len(glob.glob1("./vids","*.avi"))
     file = "./vids/demo_" + str(video_counter) + ".avi"
-    writer = cv2.VideoWriter(filename=file,  #Provide a file to write the video to
+    if record:
+        writer = cv2.VideoWriter(filename=file,  #Provide a file to write the video to
                              fourcc=cv2.cv.CV_FOURCC('P','I','M','1'),            #bring up codec dialog box
                              fps=30,
                              frameSize=(640, 480))
@@ -87,4 +92,6 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         print "KeyboardInterrupt detected."
     cv2.destroyAllWindows()
-    writer.release()
+    if record:
+        writer.release()
+        
