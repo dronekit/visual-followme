dae31c0548f6b0075b6d08334a7076c59b90c800
Arthur Benemann <arthur.benemann@gmail.com>
2014-06-27 22:58:49 -0700

    bugfix: create vids folder if it doesn't exists.
    Closes #1

--- src/file_utils.py
+++ src/file_utils.py
@@ -1,6 +1,7 @@
 import cv2
 import datetime
 import glob
+import os
 
 
 def get_new_file_name(path):
@@ -12,6 +13,8 @@ def time_stamped(fname, fmt='{fname}_%Y-%m-%d-%H-%M-%S'):
     return datetime.datetime.now().strftime(fmt).format(fname=fname)
 
 def get_loggers(path = "../vids/"):
+    if not os.path.exists(path):
+        os.makedirs(path)
     filename = get_new_file_name(path)
     videoWriter = cv2.VideoWriter(filename=filename, # Provide a file to write the video to
         fourcc=cv2.cv.CV_FOURCC('P', 'I', 'M', '1'), # bring up codec dialog box
