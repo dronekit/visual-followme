33c8193368e11a3e1e08daf65bc11df6da16a2dc
Arthur Benemann <arthur.benemann@gmail.com>
2014-06-28 16:52:48 -0700

    Draw detected position on screen

--- src/gui.py
+++ src/gui.py
@@ -1,15 +1,17 @@
 import cv2
 
 
-def render_crosshairs(frame, biggest_contour):
-    if biggest_contour != None:
+def render_crosshairs(frame, target):
+    if target != None:
         green = 20, 255, 60
-        contour_centroid = cv2.moments(biggest_contour)
+        contour_centroid = cv2.moments(target)
         
         try:
             cx, cy = int(contour_centroid['m10'] / contour_centroid['m00']), int(contour_centroid['m01'] / contour_centroid['m00'])
             cv2.circle(frame, (cx, cy), 4, green, 5)
             cv2.line(frame, (0, cy), (frame.shape[1], cy), green, 1)
             cv2.line(frame, (cx, 0), (cx, frame.shape[0]), green, 1)
+            
+            cv2.putText(frame,'Y:%d'%cy,(0,25), cv2.FONT_HERSHEY_SIMPLEX, 1,(0,0,0),2)
         except ZeroDivisionError:
             pass
