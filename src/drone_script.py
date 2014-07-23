b95cc6e656d685b81f76c016d22388d0672a8899
Kevin Hester <kevinh@geeksville.com>
2014-07-23 10:33:28 -0700

    Iterate over cameras until we find one that works

--- src/drone_script.py
+++ src/drone_script.py
@@ -1,6 +1,7 @@
 import cv2
 import time
 import os.path
+import os
 
 from file_utils import Logger
 from polyphemus import process_stream
@@ -16,14 +17,25 @@ def wait_for_arm(v):
     print "ARMED"
 
 
+def open_camera():
+    # yuck - opencv has no way to count # of cameras, so do this hack of looking for /dev/video*
+    numCameras = len(filter(lambda s: s.startswith("video"), os.listdir("/dev")))
+
+    c = cv2.VideoCapture()
+    for cnum in range(0, numCameras):
+        c.open(0)
+        if c.isOpened():
+            return c
+
+    raise Exception('No cameras found')
+
 print "DroneScript - Visual-Follow Running"
 v = get_vehicle()
 
 while True:
     wait_for_arm(v)    
 
-    video_in = cv2.VideoCapture()
-    video_in.open(0)
+    video_in = open_camera()
     
     homedir = os.path.expanduser("~")
     logger = Logger(path= homedir + "/Videos/")
